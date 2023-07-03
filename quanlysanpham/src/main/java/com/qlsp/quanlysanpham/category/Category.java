c978f79d77ec9050621e3a9f198dda696eee8cc4
Giang03 <89713275+giang03@users.noreply.github.com>
2023-07-03 16:03:56 +0700

    Khởi tạo project và model

---
+++ quanlysanpham/src/main/java/com/qlsp/quanlysanpham/category/Category.java
@@ -0,0 +1,6 @@
+package com.qlsp.quanlysanpham.category;
+
+public class Category {
+    private Integer id;
+    private String name;
+}
---
+++ quanlysanpham/src/main/java/com/qlsp/quanlysanpham/product/Product.java
@@ -0,0 +1,9 @@
+package com.qlsp.quanlysanpham.product;
+
+import com.qlsp.quanlysanpham.category.Category;
+
+public class Product {
+    private Integer id;
+    private String name;
+    private Category category;
+}
